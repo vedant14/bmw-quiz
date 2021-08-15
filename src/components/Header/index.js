4a2448546d616b016c2a6a589289d024fc39e33e
Vedant Lohbare <vedant.lohbare@gmail.com>
2021-08-15 17:01:48 +0530

    Small change on the url

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export function Header() {
 		<Wrapper className="Container">
 			<a
 				class="twitter-share-button"
-				href="https://twitter.com/intent/tweet?text=Check%20salaries%20at%20Unicorns&url=http://howmanybmws.com/"
+				href="https://twitter.com/intent/tweet?text=Check%20salaries%20at%20Unicorns&url=http://howmanyBMWs.com/"
 				data-size="large"
 				target="_blank"
 				rel="noreferrer"
