cc6d6e86767f421abb719d01aca8375b333bb599
Vedant Lohbare <vedant.lohbare@gmail.com>
2021-08-15 17:06:23 +0530

    Social image change

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ const IndexPage = ({ data }) => {
 
 export const query = graphql`
   {
-    file(name: { eq: "social-card" }) {
+    file(name: { eq: "social-card-twitter" }) {
       name
       childrenImageSharp {
         resize(width: 1200) {
